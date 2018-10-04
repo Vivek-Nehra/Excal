e98fa0eb6ed1ab4c730f62776d36cf202f10ee48
Vivek-Nehra <nehravivek2@gmail.com>
2018-10-04 18:04:28 +0530

    Improvised changes to horizontal lines

--- Testing.py
+++ Testing.py
@@ -6,7 +6,7 @@ import math
 def deletelines(lines):
     for line in lines:
         for index,line2 in enumerate(lines):
-            if abs(line[0][1] - line2[0][1]) < 1:
+            if abs(line[0][1] - line2[0][1]) < 20:
                 #print (abs(line[0][1] - line2[0][1]))
                 del lines[index]
     return lines
@@ -37,7 +37,7 @@ if lines is not None:
     for line in lines:
         if abs(line[0][1] - line[0][3]) < 10:
             val +=1
-            cv2.line(img,(line[0][0],line[0][1]),(line[0][2],line[0][3]),(0,255,0),3)
+            cv2.line(img,(line[0][0],line[0][1]),(line[0][2],line[0][3]),(0,255,0),1)
 print(val)
 cv2.imshow("Original",img)
 cv2.imshow("Sobel",sobel)
